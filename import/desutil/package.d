1e1b59b0780e3e2498cd53516e8088891619d4ea
deviator <code.viator@gmail.com>
2014-06-13 00:55:40 +0400

    move math util il to core

--- import/desutil/package.d
+++
@@ -1,9 +0,0 @@
-module desutil;
-
-public import desutil.helpers;
-public import desutil.langpack;
-public import desutil.logger;
-public import desutil.signal;
-public import desutil.timer;
-public import desutil.desml;
-public import desutil.socket;
