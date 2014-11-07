800879291c977744f0d66e8213b40142e092286d
deviator <code.viator@gmail.com>
2014-11-07 05:51:48 +0300

    move DrawMode enum of GLSimpleObject to public

--- import/des/gl/simple/object.d
+++ import/des/gl/simple/object.d
@@ -100,21 +100,6 @@ protected:
         return buf;
     }
 
-    enum DrawMode
-    {
-        POINTS = GL_POINTS,
-        LINE_STRIP = GL_LINE_STRIP,
-        LINE_LOOP = GL_LINE_LOOP,
-        LINES = GL_LINES,
-        LINE_STRIP_ADJACENCY = GL_LINE_STRIP_ADJACENCY,
-        LINES_ADJACENCY = GL_LINES_ADJACENCY,
-        TRIANGLE_STRIP = GL_TRIANGLE_STRIP,
-        TRIANGLE_FAN = GL_TRIANGLE_FAN,
-        TRIANGLES = GL_TRIANGLES,
-        TRIANGLE_STRIP_ADJACENCY = GL_TRIANGLE_STRIP_ADJACENCY,
-        TRIANGLES_ADJACENCY = GL_TRIANGLES_ADJACENCY,
-    }
-
     bool draw_flag = true;
 
     void preDraw()
@@ -158,5 +143,19 @@ public:
     this( CommonShaderProgram sh )
     in{ assert( sh !is null ); } body
     { shader = sh; }
-}
 
+    enum DrawMode
+    {
+        POINTS = GL_POINTS,
+        LINE_STRIP = GL_LINE_STRIP,
+        LINE_LOOP = GL_LINE_LOOP,
+        LINES = GL_LINES,
+        LINE_STRIP_ADJACENCY = GL_LINE_STRIP_ADJACENCY,
+        LINES_ADJACENCY = GL_LINES_ADJACENCY,
+        TRIANGLE_STRIP = GL_TRIANGLE_STRIP,
+        TRIANGLE_FAN = GL_TRIANGLE_FAN,
+        TRIANGLES = GL_TRIANGLES,
+        TRIANGLE_STRIP_ADJACENCY = GL_TRIANGLE_STRIP_ADJACENCY,
+        TRIANGLES_ADJACENCY = GL_TRIANGLES_ADJACENCY,
+    }
+}
