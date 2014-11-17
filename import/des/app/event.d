4ad552c567dc0d66933fbe20e1e16ac399fe7af7
deviator <code.viator@gmail.com>
2014-11-17 04:05:46 +0300

    no changes

--- import/des/app/event.d
+++ import/des/app/event.d
@@ -278,22 +278,22 @@ struct KeyboardEvent
     /++ модификаторы (SHIFT, ALT etc) +/
     enum Mod
     {
-        NONE = 0x0000,
+        NONE   = 0x0000,
         LSHIFT = 0x0001,
         RSHIFT = 0x0002,
-        LCTRL = 0x0040,
-        RCTRL = 0x0080,
-        LALT = 0x0100,
-        RALT = 0x0200,
-        LGUI = 0x0400,
-        RGUI = 0x0800,
-        NUM = 0x1000,
-        CAPS = 0x2000,
-        MODE = 0x4000,
-        CTRL = (LCTRL|RCTRL),
-        SHIFT = (LSHIFT|RSHIFT),
-        ALT = (LALT|RALT),
-        GUI = (LGUI|RGUI),
+        LCTRL  = 0x0040,
+        RCTRL  = 0x0080,
+        LALT   = 0x0100,
+        RALT   = 0x0200,
+        LGUI   = 0x0400,
+        RGUI   = 0x0800,
+        NUM    = 0x1000,
+        CAPS   = 0x2000,
+        MODE   = 0x4000,
+        CTRL   = (LCTRL|RCTRL),
+        SHIFT  = (LSHIFT|RSHIFT),
+        ALT    = (LALT|RALT),
+        GUI    = (LGUI|RGUI),
     }
     /++ нажата или отпущена клавиша +/
     bool pressed;
