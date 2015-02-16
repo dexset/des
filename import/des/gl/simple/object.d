47bd750790e04b0e80231d430c368b864e36e2b6
deviator <code.viator@gmail.com>
2015-02-17 02:44:36 +0300

    DrawMode moved to GLObject

--- import/des/gl/simple/object.d
+++ import/des/gl/simple/object.d
@@ -10,18 +10,18 @@ public import des.gl.base;
 abstract class GLSimpleObject : GLObject
 {
 private:
-    size_t draw_count;
-    size_t index_count;
+    uint draw_count;
+    uint index_count;
 
     GLBuffer elem_buffer = null;
 
 protected:
 
     ///
-    void setDrawCount( size_t cnt ) { draw_count = cnt; }
+    void setDrawCount( uint cnt ) { draw_count = cnt; }
 
     ///
-    void setIndexCount( size_t cnt ) { index_count = cnt; }
+    void setIndexCount( uint cnt ) { index_count = cnt; }
 
     ///
     CommonGLShaderProgram shader;
@@ -100,7 +100,7 @@ protected:
     {
         auto buf = newEMM!GLBuffer( GLBuffer.Target.ELEMENT_ARRAY_BUFFER );
         connect( buf.elementCountCB, &setIndexCount );
-        connect( buf.elementSizeCB, (size_t sz){
+        connect( buf.elementSizeCB, (uint sz){
             enforce( sz == uint.sizeof, "set to index buffer not uint data" );
         });
         elem_buffer = buf;
@@ -108,12 +108,8 @@ protected:
     }
 
     ///
-    bool draw_flag = true;
-
-    ///
-    void preDraw()
+    override void preDraw()
     {
-        vao.bind();
         shader.use();
         if( elem_buffer !is null )
             elem_buffer.bind();
@@ -123,29 +119,21 @@ protected:
     ///
     void drawArrays( DrawMode mode )
     {
-        if( !draw_flag ) return;
-        preDraw();
+        if( !visible ) return;
         if( draw_count > 0 )
-            glDrawArrays( mode, 0, cast(uint)draw_count );
+            super.drawArrays( mode, cast(uint)draw_count );
         else if( warn_if_empty )
             logger.warn( "simple object draw empty object" );
-
-        debug checkGL;
-        debug logger.trace( "mode [%s], count [%d]", mode, draw_count );
     }
 
     ///
     void drawElements( DrawMode mode )
     {
-        if( !draw_flag ) return;
-        preDraw();
+        if( !visible ) return;
         if( index_count > 0 && draw_count > 0 )
-            glDrawElements( mode, cast(uint)index_count, GL_UNSIGNED_INT, null );
+            super.drawElements( mode, cast(uint)index_count );
         else if( warn_if_empty )
             logger.warn( "simple object draw empty object" );
-
-        debug checkGL;
-        debug logger.trace( "mode [%s], count [%d]", mode, index_count );
     }
 
 public:
@@ -157,23 +145,10 @@ public:
     }
 
     ///
+    bool visible = true;
+
+    ///
     this( CommonGLShaderProgram sh )
     in{ assert( sh !is null ); } body
     { shader = sh; }
-
-    ///
-    enum DrawMode
-    {
-        POINTS                   = GL_POINTS,                  /// GL_POINTS,
-        LINES                    = GL_LINES,                   /// GL_LINES,
-        LINE_STRIP               = GL_LINE_STRIP,              /// GL_LINE_STRIP,
-        LINE_LOOP                = GL_LINE_LOOP,               /// GL_LINE_LOOP,
-        TRIANGLES                = GL_TRIANGLES,               /// GL_TRIANGLES,
-        TRIANGLE_STRIP           = GL_TRIANGLE_STRIP,          /// GL_TRIANGLE_STRIP,
-        TRIANGLE_FAN             = GL_TRIANGLE_FAN,            /// GL_TRIANGLE_FAN,
-        LINES_ADJACENCY          = GL_LINES_ADJACENCY,         /// GL_LINES_ADJACENCY,
-        LINE_STRIP_ADJACENCY     = GL_LINE_STRIP_ADJACENCY,    /// GL_LINE_STRIP_ADJACENCY,
-        TRIANGLES_ADJACENCY      = GL_TRIANGLES_ADJACENCY,     /// GL_TRIANGLES_ADJACENCY,
-        TRIANGLE_STRIP_ADJACENCY = GL_TRIANGLE_STRIP_ADJACENCY /// GL_TRIANGLE_STRIP_ADJACENCY,
-    }
 }
