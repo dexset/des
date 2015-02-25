9f330a08c4f652c56d7dd35adccf23b806c110df
deviator <code.viator@gmail.com>
2015-02-25 03:40:17 +0300

    fix logging

--- import/des/gl/base/texture.d
+++ import/des/gl/base/texture.d
@@ -1,27 +1,3 @@
-/+
-The MIT License (MIT)
-
-    Copyright (c) <2013> <Oleg Butko (deviator), Anton Akzhigitov (Akzwar)>
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-+/
-
 module des.gl.base.texture;
 
 import std.string;
@@ -36,11 +12,11 @@ import std.algorithm;
 import des.util.stdext.algorithm;
 
 ///
-class GLTextureException : DesGLException 
-{ 
+class GLTextureException : DesGLException
+{
     ///
     this( string msg, string file=__FILE__, size_t line=__LINE__ ) @safe pure nothrow
-    { super( msg, file, line ); } 
+    { super( msg, file, line ); }
 }
 
 ///
@@ -102,7 +78,7 @@ protected:
 
 public:
 
-    alias CrdVector!3 texsize_t; 
+    alias CrdVector!3 texsize_t;
 
     @property
     {
@@ -291,7 +267,7 @@ public:
     {
         _unit = tu;
         checkGLCall!glGenTextures( 1, &_id );
-        logger = new InstanceLogger( this, format( "%d:%d", tu, _id ) );
+        logger = new InstanceLogger( this, format( "tu:%d, id:%d", tu, _id ) );
         _target = tg;
         logger.Debug( "with target [%s]", _target );
     }
